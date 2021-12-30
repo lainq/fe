f8592b1c00dd320fe34dceb5c79155fa5f32c524
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-12-30 12:38:06 +0530

    fmt:go fmt

--- main.go
+++ main.go
@@ -1,87 +1,87 @@
 package main
 
 import (
-  "os"
-  "strings"
-  "path"
+	"os"
+	"path"
+	"strings"
 
-  "io/ioutil"
+	"io/ioutil"
 
-  "github.com/fatih/color"
-  "github.com/manifoldco/promptui"
-  "github.com/alecthomas/chroma/quick"
-  "github.com/inancgumus/screen"
+	"github.com/alecthomas/chroma/quick"
+	"github.com/fatih/color"
+	"github.com/inancgumus/screen"
+	"github.com/manifoldco/promptui"
 )
 
 func CreateError(error string, isFatal bool) {
-  color.Red(error)
-  if isFatal {
-    os.Exit(1)
-  }
+	color.Red(error)
+	if isFatal {
+		os.Exit(1)
+	}
 }
 
 type Fe struct {
-  filepath string
+	filepath string
 }
 
 func (fe Fe) OpenDirectory(filepath string) {
-  files, err := ioutil.ReadDir(filepath)
-  if err != nil {
-    CreateError(err.Error(), true)
-  }
+	files, err := ioutil.ReadDir(filepath)
+	if err != nil {
+		CreateError(err.Error(), true)
+	}
 
-  filenames := []string{".."}
-  for _, file := range files {
-    name := file.Name()
-    filenames = append(filenames, name)
-  }
-  prompt := promptui.Select{
-    Label: "Select file",
-    Items: filenames,
-  }
-  _, result, err := prompt.Run()
-  if err != nil {
-    CreateError(err.Error(), true)
-  }
-  fe.Open(path.Join(filepath, result), true)
+	filenames := []string{".."}
+	for _, file := range files {
+		name := file.Name()
+		filenames = append(filenames, name)
+	}
+	prompt := promptui.Select{
+		Label: "Select file",
+		Items: filenames,
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		CreateError(err.Error(), true)
+	}
+	fe.Open(path.Join(filepath, result), true)
 }
 
 func (fe Fe) OpenFile(filepath string) {
-  content, err := ioutil.ReadFile(filepath)
-  if err != nil {
-    CreateError(err.Error(), true)
-  }
-  slices := strings.Split(path.Base(filepath), ".")
-  extension := slices[len(slices) - 1]
-  quick.Highlight(os.Stdout, string(content), extension, "terminal256", "monokai")
-  fe.Open(path.Dir(filepath), false)
+	content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		CreateError(err.Error(), true)
+	}
+	slices := strings.Split(path.Base(filepath), ".")
+	extension := slices[len(slices)-1]
+	quick.Highlight(os.Stdout, string(content), extension, "terminal256", "monokai")
+	fe.Open(path.Dir(filepath), false)
 }
 
 func (fe Fe) Open(filepath string, cls bool) {
-  if cls {
-    screen.Clear()
-    screen.MoveTopLeft()
-  }
-  stat, err := os.Stat(filepath)
-  if err != nil {
-    CreateError(err.Error(), true)
-  }
-  if stat.IsDir() {
-    fe.OpenDirectory(filepath)
-  } else {
-    fe.OpenFile(filepath)
-  }
+	if cls {
+		screen.Clear()
+		screen.MoveTopLeft()
+	}
+	stat, err := os.Stat(filepath)
+	if err != nil {
+		CreateError(err.Error(), true)
+	}
+	if stat.IsDir() {
+		fe.OpenDirectory(filepath)
+	} else {
+		fe.OpenFile(filepath)
+	}
 }
 
 func main() {
-  args := os.Args[1:]
-  path, err := os.Getwd()
-  if err != nil {
-    CreateError(err.Error(), true)
-  }
-  if len(args) > 0 {
-    path = args[0]
-  }
-  fe := Fe{path}
-  fe.Open(path, false)
+	args := os.Args[1:]
+	path, err := os.Getwd()
+	if err != nil {
+		CreateError(err.Error(), true)
+	}
+	if len(args) > 0 {
+		path = args[0]
+	}
+	fe := Fe{path}
+	fe.Open(path, false)
 }
